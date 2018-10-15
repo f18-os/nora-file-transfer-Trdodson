6bc178a93ff5e9c59955eb80f317f6598c3deb1e
Taylor Dodson <t.raynedod@gmail.com>
2018-10-15 10:32:49 -0600

    Can now specify a destination namne different from file name.

--- threaded-file-transfer-lab/threadClient.py
+++ threaded-file-transfer-lab/threadClient.py
@@ -12,12 +12,14 @@ switchesVarDefaults = (
     (('-d', '--debug'), "debug", False), # boolean (set if present)
     (('-?', '--usage'), "usage", False),
     (('-f', '--file'), 'file', "default.txt"), # File parameter.
+    (('-dst', '--destination'), 'destination', "default"), #File destination parameter
     )
 
 progname = "threadClient"
 paramMap = params.parseParams(switchesVarDefaults)
 
-server, usage, debug, fileName = paramMap["server"], paramMap["usage"], paramMap["debug"], paramMap["file"]
+server, usage, debug, fileName, dstName = paramMap["server"], paramMap["usage"], paramMap["debug"], paramMap["file"], paramMap["destination"]
+print(fileName, dstName)
 
 if usage:
     params.usage()
@@ -35,6 +37,7 @@ class ClientThread(Thread):
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
+      # Open up socket. 
        s = None
        for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
            af, socktype, proto, canonname, sa = res
@@ -59,6 +62,7 @@ class ClientThread(Thread):
            print('could not open socket')
            sys.exit(1)
 
+       # Once socket is open, we begin.
        print("Success! Trying to open %s" % fileName)
        
        try:
@@ -69,11 +73,15 @@ class ClientThread(Thread):
                print("ERROR: File doesn't exist. Stopping...")
                fs.sendmsg(b"error") # Send Error if file doesn't exist.
                s.close()
-               exit()
+               exit(1)
 
            fs.sendmsg(b"accept")    # Send an accept if everything is fine!
+
+           if (dstName != "default"): # The desired destination may not be the same as the chosen file: check!
+               fs.sendmsg(dstName.encode())
+           else:
+               fs.sendmsg(fileName.encode()) # Send file name.
         
-           fs.sendmsg(fileName.encode()) # Send file name.
            fs.sendmsg(b"") # Send this so they know they got it all!
            
            print("sending %s" % fileName)
@@ -83,9 +91,11 @@ class ClientThread(Thread):
                fs.sendmsg(line)    # Send file line by line.
                line = myFile.read(100)
            myFile.close()
+           s.close()
            print("%s sent." % fileName)
        except:
            print("ERROR: Broke connection. Exiting...")
+           s.close()
            exit()
 
 for i in range(1):
